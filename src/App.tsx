58a269d1ebbcf060e4776c5a8b0271daebe2a963
BartekDygasinski-TomTom <bartlomiej.dygasinski@tomtom.com>
2025-07-22 18:53:20 +0200

    Added Tombac UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { Button, Heading, Text, TombacApp } from "tombac";
 import React from "react";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <TombacApp>
+      <Heading level={1}>Hello world!</Heading>
+      <Text>Lorem ipsum dolor sit amet.</Text>
+      <Button onClick={() => alert("Thank you!")} variant="primary">
+        Click me
+      </Button>
+    </TombacApp>
+  );
 };
 
 const container = document.getElementById("root");
